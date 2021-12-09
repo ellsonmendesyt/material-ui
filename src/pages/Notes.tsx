431848b35f8dd0301fd451cda928b80b28253a80
ellsonmendesyt <elsonmendesoficial@gmail.com>
2021-12-09 14:35:25 -0300

    tipografia

---
+++ src/pages/Notes.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const Notes = () => {
+    return (
+        <div>
+          <Typography variant='body1' color='primary' align='center' component='h1' >Hello</Typography>
+          <Typography color='secondary' align='right' variant='body2' component='h1' >Hello</Typography>
+           <Typography noWrap align='center' color='primary' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quidem fugiat repudiandae temporibus, dolorum nostrum voluptas tempora aliquam inventore fuga?</Typography>
+        
+        </div>
+    )
+}
+
+export default Notes
